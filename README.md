6fa702bed4e78e97a32b544289a5b992b686d0fb
amitshri777 <amitshri777@gmail.com>
2022-06-22 17:35:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-m1qna8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vite-m1qna8)
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = (): JSX.Element => <h1>ENV MODE: {import.meta.env.MODE}</h1>;
+
+export default App;
